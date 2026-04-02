5d6db719786c5c938c0b6183df332b252fe56fd3
GONABR1 <fefebairos@gmail.com>
2026-04-02 15:59:34 -0300

    Adicionando classe Avaliacao

---
+++ praiou/src/main/java/br/gov/sp/fatec/pg/praiou/model/Avaliacao.java
@@ -0,0 +1,48 @@
+package br.gov.sp.fatec.pg.praiou.model;
+
+import java.util.Date;
+
+public class Avaliacao {
+
+    private Integer id;
+    private Usuario avaliador; 
+    private Evento evento;     // O EVENTO que está sendo avaliado
+    private boolean curtida;   
+    private Date dataAvaliacao;
+
+    // Construtor Vazio
+    public Avaliacao() {}
+
+    // Construtor Básico 
+    public Avaliacao(Usuario avaliador, Evento evento, boolean curtida) {
+        this.avaliador = avaliador;
+        this.evento = evento;
+        this.curtida = curtida;
+        this.dataAvaliacao = new Date();
+    }
+
+    // Construtor Completo 
+    public Avaliacao(Integer id, Usuario avaliador, Evento evento, boolean curtida, Date dataAvaliacao) {
+        this.id = id;
+        this.avaliador = avaliador;
+        this.evento = evento;
+        this.curtida = curtida;
+        this.dataAvaliacao = dataAvaliacao;
+    }
+
+    // Getters e Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public Usuario getAvaliador() { return avaliador; }
+    public void setAvaliador(Usuario avaliador) { this.avaliador = avaliador; }
+
+    public Evento getEvento() { return evento; }
+    public void setEvento(Evento evento) { this.evento = evento; }
+
+    public boolean isCurtida() { return curtida; }
+    public void setCurtida(boolean curtida) { this.curtida = curtida; }
+
+    public Date getDataAvaliacao() { return dataAvaliacao; }
+    public void setDataAvaliacao(Date dataAvaliacao) { this.dataAvaliacao = dataAvaliacao; }
+}
